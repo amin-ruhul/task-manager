d33af79b6ab17014733bd921cd8445b5f4a9d66e
amin_ruhul <ruhulinfo50@gmail.com>
2021-01-19 13:05:21 +0600

    add icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 function Task({ task }) {
   return (
     <div className="task">
-      <h3>{task.title}</h3>
+      <h3>
+        {task.title} <FaTimes style={{ color: "red" }} />
+      </h3>
       <p>{task.date}</p>
     </div>
   );
