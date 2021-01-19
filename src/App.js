cd4976379101bf513935608befa021491d1e8d63
amin_ruhul <ruhulinfo50@gmail.com>
2021-01-19 14:48:52 +0600

    delete task

--- src/App.js
+++ src/App.js
@@ -19,10 +19,17 @@ function App() {
       date: "19-jan-2021",
     },
   ]);
+  const handelDelete = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={handelDelete} />
+      ) : (
+        "Empty.."
+      )}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 
-function Task({ task }) {
+function Task({ task, onDelete }) {
   return (
     <div className="task">
       <h3>
-        {task.title} <FaTimes style={{ color: "red" }} />
+        {task.title}
+        <FaTimes style={{ color: "red" }} onClick={onDelete} />
       </h3>
       <p>{task.date}</p>
     </div>
