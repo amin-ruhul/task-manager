a762b3642c77812d9c1d81810f395d1db8ad7da3
amin_ruhul <ruhulinfo50@gmail.com>
2021-01-19 12:41:35 +0600

    added compo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "task-1",
+      date: "19-jan-2021",
+    },
+    {
+      id: 2,
+      title: "task-2",
+      date: "19-jan-2021",
+    },
+    {
+      id: 3,
+      title: "task-3",
+      date: "19-jan-2021",
+    },
+  ]);
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from "./Task";
+
+function Tasks({ tasks }) {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} />
+      ))}
+    </>
+  );
+}
+
+export default Tasks;
