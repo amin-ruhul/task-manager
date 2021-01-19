02cf825aa76c40b1fac258ac7a5d22b1a1e9dccd
amin_ruhul <ruhulinfo50@gmail.com>
2021-01-19 12:19:56 +0600

    added component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Button from "./Button";
 
-function Header() {
+function Header({ title }) {
   return (
-    <div>
-      <h1>Task Manager</h1>
+    <div className="header">
+      <h1>{title}</h1>
+      <Button text="Add" color="green" />
     </div>
   );
 }
+
+Header.defaultProps = {
+  title: "Task Manager",
+};
 
 export default Header;
